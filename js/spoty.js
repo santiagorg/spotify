7360e142f4da742e6f1e6f061c25c24aa34e7978
santiagorg <yami_n2o@hotmail.com>
2017-07-18 18:14:51 +0200

    js searchArtist

---
+++ js/spoty.js
@@ -0,0 +1,18 @@
+
+(function($){
+	var $btn = $('#request');
+	var $artist = $('#artist');
+
+	$btn.on('click', function(){
+		$artist.load('https://api.spotify.com/v1/search?type=artist&query=SEARCHTERM', completeFunction);
+	});
+
+	function completeFunction(responseText,textStatus, request){
+		$artist.css('border','1px solid pink');
+		console.log(request);
+		if(textStatus === 'error'){
+			$artist.text('Error al mostrar artista'+request.status+' '+request.statusText);
+		}
+	}
+
+})(jQuery);
